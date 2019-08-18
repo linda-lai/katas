bece27574264f815306b7fdb57b142b7e801ea22
Linda Lai <lindalai.615@gmail.com>
2019-08-18 18:24:19 +1000

    NEW: Add DNAtoRNA function and install ESLint package

---
+++ dna_to_rna_conversion.js
@@ -0,0 +1,19 @@
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+// DNAtoRNA("GCAT") returns ("GCAU")
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+const DNAtoRNA = (dna) => {
+  return dna.toUpperCase().split('T').join('U')
+}
+
+const DNAtoRNAWithRegex = (dna) => {
+  return dna.toUpperCase().replace(/T/gi, 'U')
+}
+
+module.exports = DNAtoRNA;
